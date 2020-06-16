b10d5108548be3df4418343d0a75f4d8596f0876
Marcin Holcman <holcmanmarcin@gmail.com>
2020-06-16 19:03:26 +0200

    Add jump and movement scripts

---
+++ Project One/Assets/Player/PlayerJump.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    [SerializeField] private LayerMask platformLeyerMask;
+    private Rigidbody2D rb;
+    private BoxCollider2D boxCollider2D;
+    public float speed = 10f;
+
+    void Start()
+    {
+        rb = transform.GetComponent<Rigidbody2D>();
+        boxCollider2D = transform.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * speed;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit2D =  Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, .1f, platformLeyerMask);
+        Debug.Log(raycastHit2D.collider);
+        return raycastHit2D.collider != null;
+    }
+}
---
+++ Project One/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), 0);
+       moveVelocity = moveInput.normalized * speed;
+    }
+
+    private void FixedUpdate()
+    {
+       if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            {
+                rb.velocity = new Vector2(+speed, rb.velocity.y);
+            }
+            else
+            {
+                rb.velocity = new Vector2(0f, rb.velocity.y);
+            }
+        }
+    }
+}
